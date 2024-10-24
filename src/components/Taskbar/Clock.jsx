20f66cddfeccf4559ced38cc85fd6517135ef8e0
Maxime J. <jennmaxime@gmail.com>
2024-10-24 22:41:20 +0200

    Locale

--- src/components/Taskbar/Clock.jsx
+++ src/components/Taskbar/Clock.jsx
@@ -6,12 +6,13 @@ const Clock = () => {
     const [Time, setTime] = useState(new Date())
     const { t } = useTranslation();
 
+    //TODO : Fix the memory leak (reset the tooltip is doing bs)
     useEffect(() => {
         const interval = setInterval(() => {
             setTime(new Date())
         }, 1000)
         return () => clearInterval(interval)
-    }, [])
+    }, [Time])
     
     const formatTime = (time) => {
         return time < 10 ? `0${time}` : time
@@ -27,11 +28,11 @@ const Clock = () => {
     return (
     <>
         <TooltipComp text={date}>
-            <span style={{fontSize: "9px", fontFamily: "PX", cursor: "default"}}>
+            <div style={{fontSize: "9px", fontFamily: "PX", cursor: "default"}}>
                 {formatTime(Time.getHours())}
                 &thinsp;:&thinsp;
                 {formatTime(Time.getMinutes())}
-            </span>
+            </div>
         </TooltipComp>
     </>
   )
